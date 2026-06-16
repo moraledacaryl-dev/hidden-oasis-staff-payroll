2cd83d688d089a9d1e7b135e086b6e420bd5cd65
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-06-16 11:18:53 +0800

    Add API package marker

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+"""Production API package for Hidden Oasis Staff Payroll.
+
+The API is introduced as a wrapper around the existing Python payroll engine.
+It must not replace or duplicate payroll calculations.
+"""
