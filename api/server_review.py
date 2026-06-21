bde982921c094d090e8339ad4852f33a09e4ac9b
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-06-22 01:30:47 +0800

    Register controlled payroll revision workflow

--- api/server_review.py
+++ api/server_review.py
@@ -9,6 +9,7 @@ from api.payroll_mark_paid import router as payroll_mark_paid_router
 from api.payroll_corrections import router as payroll_corrections_router
 from api.payroll_audit_events import router as payroll_audit_events_router
 from api.payroll_revision_controls import router as revision_controls_router
+from api.payroll_revision_workflow import router as revision_workflow_router
 from api.production_health import router as production_health_router
 from api.hr_records import router as hr_records_router
 from api.payslip_distribution import router as payslip_distribution_router
@@ -38,6 +39,7 @@ app.include_router(payroll_corrections_router)
 app.include_router(payroll_audit_events_router)
 app.include_router(schedule_migration_router)
 app.include_router(revision_controls_router)
+app.include_router(revision_workflow_router)
 app.include_router(production_health_router)
 app.include_router(hr_records_router)
 app.include_router(payslip_distribution_router)
