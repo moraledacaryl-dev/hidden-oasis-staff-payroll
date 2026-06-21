3dbbcd5d419803008cecaa0c952184a5f96777ec
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-06-22 02:39:17 +0800

    Use revision workflow with carried adjustments

--- api/server_review.py
+++ api/server_review.py
@@ -9,7 +9,7 @@ from api.payroll_mark_paid import router as payroll_mark_paid_router
 from api.payroll_corrections import router as payroll_corrections_router
 from api.payroll_audit_events import router as payroll_audit_events_router
 from api.payroll_revision_controls import router as revision_controls_router
-from api.payroll_revision_workflow import router as revision_workflow_router
+from api.payroll_revision_workflow_v2 import router as revision_workflow_router
 from api.production_health import router as production_health_router
 from api.hr_records import router as hr_records_router
 from api.payslip_distribution import router as payslip_distribution_router
