1335c0cf378895257fa25086536f6d3ab1a77b9c
moraledacaryl-dev <moraledacaryl@gmail.com>
2026-08-06 23:28:36 +0800

    refactor: migrate staff self service security imports

--- api/staff_self_service.py
+++ api/staff_self_service.py
@@ -8,7 +8,7 @@ from typing import Any
 from fastapi import APIRouter, Depends, File, Header, HTTPException, UploadFile
 from pydantic import BaseModel, Field
 
-from api.main import current_user_from_token, require_api_key
+from api.security import current_user_from_token, require_api_key
 from api.schedule_change_log import log_schedule_change
 from api.upload_validation import MAX_UPLOAD_BYTES, validate_upload_bytes
 from core.db import DB_PATH, fetchall, fetchone, get_conn
